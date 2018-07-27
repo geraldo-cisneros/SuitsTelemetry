4f0a74e2fb1051afa1197d39f5675d7f179e8bb4
geraldo-cisneros <41762712+geraldo-cisneros@users.noreply.github.com>
2018-07-27 15:50:32 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ SuitSwitch = require('./models/suitswitch');
 //Database connector
 mongoose.connect("mongodb://admin:B29R233@cluster0-shard-00-00-4mutn.mongodb.net:27017,cluster0-shard-00-01-4mutn.mongodb.net:27017,cluster0-shard-00-02-4mutn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
 //mongoose.connect('mongodb://localhost/spacesuit');
-
+s
 //EJS framework for website display
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
